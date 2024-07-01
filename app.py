fa16a707a4c70840afffc301faf360feffc85395
Lawrence Luo <luolawrence1@gmail.com>
2024-07-01 14:39:45 +0800

    working prototype

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, url_for
+import json
 
 app = Flask(__name__)
 
+def load_quickchat_data():
+	with open('static/data.json', 'r') as file:
+		return json.load(file)
 @app.route('/')
 def home():
-    return render_template('index.html')
+	data = load_quickchat_data()
+	return render_template('index.html', topics=data["topics"])
+
+@app.route('/display/<text>')
+def display(text):
+	return render_template('display.html', text=text)
 
 if __name__ == '__main__':
     app.run(debug=True)
